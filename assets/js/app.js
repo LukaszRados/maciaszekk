15acd0b8fded57261188e0249c64e7bba957398f
Lukasz Rados <lukaszrados@gmail.com>
2024-05-16 16:50:52 +0200

    feat: Align about me and 404 with figma; remove dvh polyfill

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-const resize = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-}
-
-window.addEventListener("resize", resize);
-resize()
